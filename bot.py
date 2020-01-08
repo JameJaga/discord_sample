36708cae73b8c42847ba359ed7a2e02b70e84c7d
distudy <54613530+distudy@users.noreply.github.com>
2020-01-08 17:48:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,16 @@ import os
 import discord
 from discord.ext import commands
 
+import cogs.mcount as mcount
+
 bot = commands.Bot(command_prefix='!')
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
+    mcount.setup(bot)
     print('起動しました！(\'◇\')ゞ')
+    
 @bot.command()
 async ping(ctx):
     await ctx.send('pong!')
