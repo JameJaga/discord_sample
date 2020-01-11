68768b837a2321f23fe63c712c2f1f4723bcf837
distudy <54613530+distudy@users.noreply.github.com>
2020-01-11 10:52:20 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,14 @@ import re
 import discord
 from discord.ext import commands
 import os
+import cogs.ping as ping
+import cogs.counter as counter
 
 @bot.event
 async def on_ready():
+    pint.setup(bot)
+    counter.setup(bot)
     print('起動しました！(\'◇\')ゞ')
-    print(values)
 @bot.command()
 async ping(ctx):
     await ctx.send('pong!')
