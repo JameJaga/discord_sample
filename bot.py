c4188ba809af3c8c3fbd70a8e8a4649ec13da5ba
distudy <54613530+distudy@users.noreply.github.com>
2020-01-11 10:17:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,36 @@
-import os
+# coding: UTF-8
+import re
 import discord
+import random
 from discord.ext import commands
+import os
+import asyncio
+import asyncpg
+from yarl import URL
+import pandas as pd
 
-import cogs.mcount as mcount
+#データベース
+dburl = URL(os.environ.get("DATABASE_URL"))
+host = dburl.host
+user = dburl.user
+database = dburl.path[1:]
+port = dburl.port
+password = dburl.password
+print('host = ' + str(host))
+print('user = ' + str(user))
+print('database = ' + str(database))
+print('port = ' + str(port))
+print('password = ' + str(password))
 
 bot = commands.Bot(command_prefix='!')
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
+conn = await asyncpg.connect(host = host ,user = user, database = database, port = port, password = password)
+values = await conn.fetch(f'SELECT * ')
 @bot.event
 async def on_ready():
-    mcount.setup(bot)
     print('起動しました！(\'◇\')ゞ')
-    
+    print(values)
 @bot.command()
 async ping(ctx):
     await ctx.send('pong!')
