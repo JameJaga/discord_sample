14cdb51253d026e628897f69f5723e7f086143eb
distudy <54613530+distudy@users.noreply.github.com>
2020-01-11 10:40:14 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,7 @@ import re
 import discord
 from discord.ext import commands
 import os
-from yarl import URL
-import pandas as pd
 
-#データベース
-dburl = URL(os.environ.get("DATABASE_URL"))
-host = dburl.host
-user = dburl.user
-database = dburl.path[1:]
-port = dburl.port
-password = dburl.password
-print('host = ' + str(host))
-print('user = ' + str(user))
-print('database = ' + str(database))
-print('port = ' + str(port))
-print('password = ' + str(password))
-
-bot = commands.Bot(command_prefix='!')
-TOKEN = os.environ.get("DISCORD_TOKEN")
-
-conn = await asyncpg.connect(host = host ,user = user, database = database, port = port, password = password)
-values = await conn.fetch(f'SELECT * ')
 @bot.event
 async def on_ready():
     print('起動しました！(\'◇\')ゞ')
